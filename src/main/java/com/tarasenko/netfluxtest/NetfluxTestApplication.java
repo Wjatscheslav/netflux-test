bbd3e5d59da1102d9bb440921bc2b90a1546af4d
viacheslav <vtarasenko@connecture.com>
2020-12-26 15:21:51 +0200

    Created spring boot app

---
+++ src/main/java/com/tarasenko/netfluxtest/NetfluxTestApplication.java
@@ -0,0 +1,15 @@
+package com.tarasenko.netfluxtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetfluxTestApplication
+{
+
+  public static void main(String[] args)
+  {
+    SpringApplication.run(NetfluxTestApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/tarasenko/netfluxtest/NetfluxTestApplicationTests.java
@@ -0,0 +1,15 @@
+package com.tarasenko.netfluxtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetfluxTestApplicationTests
+{
+
+  @Test
+  void contextLoads()
+  {
+  }
+
+}
